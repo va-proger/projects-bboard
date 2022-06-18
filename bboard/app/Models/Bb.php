06ff9afeba3821cdaef80c058e516e026dc61217
Vikulov Andrey <vikulov.prog@gmail.com>
2022-06-19 00:47:41 +0400

    Initial commit

---
+++ bboard/app/Models/Bb.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+class Bb extends Model
+{
+    protected $fillable = ['title', 'content', 'price'];
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ bboard/app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    protected $policies = [
+      'App\Models\Bb' => 'App\Policies\BbPolicy',
+    ];
+
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
